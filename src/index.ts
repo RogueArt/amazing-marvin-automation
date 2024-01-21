a69f5f56b2731fd04a16dc88a7279fb1e8410494
Rogue Art <rogueart.dev@outlook.com>
2024-01-21 11:55:46 -0800

    Refactor - Remove unnecessary index.ts comments

--- src/index.ts
+++ src/index.ts
@@ -7,20 +7,16 @@ import * as CONSTANTS from '../lib/constants'
 
 dotenv.config();
 
-const app = express();
+// Send API token each time to Marvin
+axios.defaults.headers.common['X-API-Token'] = process.env.MARVIN_API_TOKEN;
 
+const app = express();
 // TO-DO: Limit the scope of CORS permissions
 app.use(cors({ credentials: true, origin: true, exposedHeaders: '*' }))
-
-// Express setup
 app.use(express.json());
-
-// 1. Authenticate the axios client
-axios.defaults.headers.common['X-API-Token'] = process.env.MARVIN_API_TOKEN;
 
 app.use(habitTaskRouter);
 
-// Listen on port 8080
 app.listen(CONSTANTS.PORT, () => {
   console.log(`Server is running on port ${CONSTANTS.PORT}`);
 });
